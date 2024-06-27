6b05e91961bc761daf7f1f67cc9f288fa4a6836a
vind-123 <dubeyrajan810@gmail.com>
2024-06-27 20:16:18 +0530

    update colm value

---
+++ update.sql
@@ -0,0 +1,10 @@
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+
+--e.g
+
+update Family
+set MOBILE_NUMBER="919867576219"
+WHERE ADHAR_ID="12345";    -- it will change or update the mobile number where id =12345
