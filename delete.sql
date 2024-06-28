42dde38ce5e8b4154bb509c04e0043680b3f9c5d
vind-123 <dubeyrajan810@gmail.com>
2024-06-29 02:13:10 +0530

    del

--- delete.sql
+++ delete.sql
@@ -1,4 +1,4 @@
 /* The DELETE statement is used to delete existing records in a table. 
-DELETE Syntax
-DELETE FROM table_name WHERE condition; */
+DELETE Syntax */
+DELETE FROM table_name WHERE condition; 
 delete from rkd.family where ADHAR_ID=12345;
